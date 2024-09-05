336c0accc9c5b71d33ef2a65739f00e91dd37bb0
shinji5761 <shinji.tec.5761@gmail.com>
2024-09-06 01:17:37 +0900

    stripe delete

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,7 +1,6 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import { stripe } from "@/lib/stripe";
 
 export async function GET(request: NextRequest) {
     console.log("===================== callback ============================");
@@ -15,36 +14,28 @@ export async function GET(request: NextRequest) {
         if (error) {
             console.error(error);
         }
-    }
-    // user情報の取得
-    const {
-        data: { session },
-    } = await supabase.auth.getSession();
-    const user = session!.user;
-    const customer = await stripe.customers.create({
-        email: user.email,
-        payment_method: "pm_card_visa",
-        invoice_settings: {
-            default_payment_method: "pm_card_visa",
-        },
-    });
-    if (!customer) {
-        throw new Error("customer is null");
-    }
-
-    if (user != null && customer != null) {
-        await supabase!.from("users").insert([
-            {
-                uid: user.id,
-                email: user.email,
-                name: "",
-                role: "User",
-                member_ship: "Free",
-                delete_flag: false,
-                updated_at: null,
-                stripe_id: customer.id,
-            },
-        ]);
+        // user情報の取得
+        const {
+            data: { session },
+        } = await supabase.auth.getSession();
+        const user = session!.user;
+        console.log(user);
+        const appUser = await supabase.from("users").select("*").eq("uid", user.id).single();
+        console.log(appUser);
+        if (appUser != null) {
+            await supabase!.from("users").insert([
+                {
+                    uid: user.id,
+                    email: user.email,
+                    name: "",
+                    role: "User",
+                    member_ship: "Free",
+                    delete_flag: false,
+                    updated_at: null,
+                    stripe_id: null,
+                },
+            ]);
+        }
     }
 
     return NextResponse.redirect(requestUrl.origin);
